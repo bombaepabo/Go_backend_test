da7a593a11c11bcdcb3e5f89622681b4aecd31f2
phakkhapon aksonchanya <55224271+bombaepabo@users.noreply.github.com>
2023-06-25 16:44:18 +0700

    added connection to a postgres
    cannot go any further bc sqlc posgres not support windows

--- main.go
+++ main.go
@@ -7,24 +7,37 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 	godotenv.Load(".env")
 	portstring := os.Getenv("PORT")
-	if(portstring == ""){
+	if portstring == "" {
 		log.Fatal("Port is not found in the environment")
 	}
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	router.Mount("/v1", v1Router)
 	server := &http.Server{
 		Handler: router,
-		Addr: ":" + portstring,
+		Addr:    ":" + portstring,
 	}
-	log.Printf("Server starting on port %v ",portstring)
-	err :=server.ListenAndServe()
-	if err != nil{
+	log.Printf("Server starting on port %v ", portstring)
+	err := server.ListenAndServe()
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Port :",portstring)
+	fmt.Println("Port :", portstring)
 }
